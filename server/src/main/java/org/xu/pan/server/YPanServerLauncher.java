aa31744a99c292132e26ba4b1a37d7e43e76377b
Qingchao-Xu <15154739019@163.com>
2024-09-04 20:16:21 +0800

    配置全局入参校验器

--- server/src/main/java/org/xu/pan/server/YPanServerLauncher.java
+++ server/src/main/java/org/xu/pan/server/YPanServerLauncher.java
@@ -2,6 +2,7 @@ package org.xu.pan.server;
 
 import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xu.pan.core.constants.YPanConstants;
@@ -9,10 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.xu.pan.core.response.R;
 
+import javax.validation.constraints.NotBlank;
+
 @SpringBootApplication(scanBasePackages = YPanConstants.BASE_COMPONENT_SCAN_PATH)
 @ServletComponentScan(basePackages = YPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController
 @Api("测试接口类")
+@Validated
 public class YPanServerLauncher {
 
     public static void main(String[] args) {
@@ -20,7 +24,7 @@ public class YPanServerLauncher {
     }
 
     @GetMapping("hello")
-    public R<String> hello(String name) {
+    public R<String> hello(@NotBlank(message = "name不能为空") String name) {
         return R.success("hello" + name + "!");
     }
 
