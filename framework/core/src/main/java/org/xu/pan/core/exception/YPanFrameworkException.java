2ff84fec41ea4e169bf58d8bf1a3c6778f628532
Qingchao-Xu <15154739019@163.com>
2024-09-05 14:41:24 +0800

    实现定时任务模块

---
+++ framework/core/src/main/java/org/xu/pan/core/exception/YPanFrameworkException.java
@@ -0,0 +1,10 @@
+package org.xu.pan.core.exception;
+
+/**
+ * 技术层面的异常对象
+ */
+public class YPanFrameworkException extends RuntimeException {
+    public YPanFrameworkException(String message) {
+        super(message);
+    }
+}
