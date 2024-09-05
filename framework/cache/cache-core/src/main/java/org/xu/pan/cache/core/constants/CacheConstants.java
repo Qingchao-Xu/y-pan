fae792f939c8286778e33abf31344dc7282de109
Qingchao-Xu <15154739019@163.com>
2024-09-05 13:40:47 +0800

    集成caffeine缓存

---
+++ framework/cache/cache-core/src/main/java/org/xu/pan/cache/core/constants/CacheConstants.java
@@ -0,0 +1,13 @@
+package org.xu.pan.cache.core.constants;
+
+/**
+ * Y Pan 服务端公用缓存常量类
+ */
+public interface CacheConstants {
+
+    /**
+     * Y Pan 服务端公用缓存名称
+     */
+    String Y_PAN_CACHE_NAME = "Y_PAN_CACHE";
+
+}
