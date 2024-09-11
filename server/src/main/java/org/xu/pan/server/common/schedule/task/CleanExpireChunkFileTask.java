a33cb1f1204307bfcbe6f4b70b8d256776efb0c1
Qingchao-Xu <15154739019@163.com>
2024-09-11 10:47:14 +0800

    定时任务清理过期的分片文件

---
+++ server/src/main/java/org/xu/pan/server/common/schedule/task/CleanExpireChunkFileTask.java
@@ -0,0 +1,137 @@
+package org.xu.pan.server.common.schedule.task;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.xu.pan.core.constants.YPanConstants;
+import org.xu.pan.schedule.ScheduleTask;
+import org.xu.pan.server.common.event.log.ErrorLogEvent;
+import org.xu.pan.server.modules.file.entity.YPanFileChunk;
+import org.xu.pan.server.modules.file.service.IFileChunkService;
+import org.xu.pan.storage.engine.core.StorageEngine;
+import org.xu.pan.storage.engine.core.context.DeleteFileContext;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 过期分片清理任务
+ */
+@Component
+@Slf4j
+public class CleanExpireChunkFileTask implements ScheduleTask, ApplicationContextAware {
+
+    private static final Long BATCH_SIZE = 500L;
+
+    @Autowired
+    private IFileChunkService iFileChunkService;
+
+    @Autowired
+    private StorageEngine storageEngine;
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取定时任务的名称
+     *
+     * @return
+     */
+    @Override
+    public String getName() {
+        return "CleanExpireChunkFileTask";
+    }
+
+    /**
+     * 执行清理任务
+     * <p>
+     * 1、滚动查询过期的文件分片
+     * 2、删除物理文件（委托文件存储引擎去实现）
+     * 3、删除过期文件分片的记录信息
+     * 4、重置上次查询的最大文件分片记录ID，继续滚动查询
+     */
+    @Override
+    public void run() {
+        log.info("{} start clean expire chunk file...", getName());
+
+        List<YPanFileChunk> expireFileChunkRecords;
+        Long scrollPointer = 1L;
+
+        do {
+            expireFileChunkRecords = scrollQueryExpireFileChunkRecords(scrollPointer);
+            if (CollectionUtils.isNotEmpty(expireFileChunkRecords)) {
+                deleteRealChunkFiles(expireFileChunkRecords);
+                List<Long> idList = deleteChunkFileRecords(expireFileChunkRecords);
+                scrollPointer = Collections.max(idList);
+            }
+        } while (CollectionUtils.isNotEmpty(expireFileChunkRecords));
+
+        log.info("{} finish clean expire chunk file...", getName());
+    }
+
+    /*********************************************private*********************************************/
+
+    /**
+     * 滚动查询过期的文件分片记录
+     *
+     * @param scrollPointer
+     * @return
+     */
+    private List<YPanFileChunk> scrollQueryExpireFileChunkRecords(Long scrollPointer) {
+        QueryWrapper queryWrapper = Wrappers.query();
+        queryWrapper.le("expiration_time", new Date());
+        queryWrapper.ge("id", scrollPointer);
+        queryWrapper.last(" limit " + BATCH_SIZE);
+        return iFileChunkService.list(queryWrapper);
+    }
+
+    /**
+     * 物理删除过期的文件分片文件实体
+     *
+     * @param expireFileChunkRecords
+     */
+    private void deleteRealChunkFiles(List<YPanFileChunk> expireFileChunkRecords) {
+        DeleteFileContext deleteFileContext = new DeleteFileContext();
+        List<String> realPaths = expireFileChunkRecords.stream().map(YPanFileChunk::getRealPath).collect(Collectors.toList());
+        deleteFileContext.setRealFilePathList(realPaths);
+        try {
+            storageEngine.delete(deleteFileContext);
+        } catch (IOException e) {
+            saveErrorLog(realPaths);
+        }
+    }
+
+    /**
+     * @param realPaths
+     */
+    private void saveErrorLog(List<String> realPaths) {
+        ErrorLogEvent event = new ErrorLogEvent(this, "文件物理删除失败，请手动执行文件删除！文件路径为：" + JSON.toJSONString(realPaths), YPanConstants.ZERO_LONG);
+        applicationContext.publishEvent(event);
+    }
+
+    /**
+     * 删除过期文件分片记录
+     *
+     * @param expireFileChunkRecords
+     * @return
+     */
+    private List<Long> deleteChunkFileRecords(List<YPanFileChunk> expireFileChunkRecords) {
+        List<Long> idList = expireFileChunkRecords.stream().map(YPanFileChunk::getId).collect(Collectors.toList());
+        iFileChunkService.removeByIds(idList);
+        return idList;
+    }
+
+}
